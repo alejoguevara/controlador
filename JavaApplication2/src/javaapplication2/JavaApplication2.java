1b81665c1d87d391e82aebdd67e08bf3e4e6aa18
Usuario <Usuario@Alejo-PC>
2013-10-06 19:20:27 -0500

    primera modificacion

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -14,6 +14,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Alejandro Guevara");
+        System.out.println("Quinto Sistemas");
     }
 }
